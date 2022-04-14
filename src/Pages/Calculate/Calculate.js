59f16aa1721848deb4eb50b06c6c847bf0b1f75a
Matheo Deknuydt <matheodeknuydt@mbp-de-matheo.home>
2022-04-15 00:22:55 +0200

    conf: json for data of calculate

--- src/Pages/Calculate/Calculate.js
+++ src/Pages/Calculate/Calculate.js
@@ -1,29 +1,11 @@
 import { useState } from 'react'
 import Button from './../../Components/Button/Button'
+import json from '../../Data/calculate.json'
 import './Calculate.css'
 
 const Calculate = () => {
   const [calcul, setCalcul] = useState([])
   const [result, setResult] = useState('')
-
-  const test = [
-    { value: '7', type: 'secondary' },
-    { value: '8', type: 'secondary' },
-    { value: '9', type: 'secondary' },
-    { value: '/', type: 'primary' },
-    { value: '4', type: 'secondary' },
-    { value: '5', type: 'secondary' },
-    { value: '6', type: 'secondary' },
-    { value: '*', type: 'primary' },
-    { value: '1', type: 'secondary' },
-    { value: '2', type: 'secondary' },
-    { value: '3', type: 'secondary' },
-    { value: '+', type: 'primary' },
-    { value: '0', type: 'secondary' },
-    { value: 'C', type: 'primary' },
-    { value: '=', type: 'primary' },
-    { value: '-', type: 'primary' },
-  ]
 
   const handleClick = (e) => {
     // soit on clique sur un chiffre
@@ -55,7 +37,8 @@ const Calculate = () => {
         setCalcul(calcul.splice(-1, 1))
       }
       // gestion de la multiplication/division
-      // ex : 2 + 2 * 4 = 16 car on enchaine 2 + 2 = 4 puis 4 * 4 = 16 le vrai calcul est donc (2 + 2) * 4 = 16
+      // ex : 2 + 2 * 4 = 16 car on enchaine 2 + 2 = 4 => 4 * 4 = 16 
+      // le vrai calcul est donc (2 + 2) * 4 = 16
       if (e.target.value === '*' || e.target.value === '/') {
         setCalcul([eval(calcul.join('')), e.target.value])
       } else if (e.target.value === '+' || e.target.value === '-') {
@@ -70,7 +53,7 @@ const Calculate = () => {
         <div className="calculate__display">
           <input type="text" className="calculate__display--input" value={result} readOnly/>
           <div className='calculate__display--container'>
-            {test.map((item, index) => (
+            {json.calculate.map((item, index) => (
               <Button key={index} text={item.value} type={item.type} click={handleClick} />
             ))}
           </div>
