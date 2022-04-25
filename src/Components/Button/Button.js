6dc8facf50a41b69a6a67b754478f3c64442229d
Matheo Deknuydt <matheodeknuydt@MacBook-Pro-de-Matheo.local>
2022-04-25 14:16:30 +0200

    style: modified style & add media query for modbile responsive

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -2,30 +2,17 @@ import { useEffect, useState } from "react"
 import './Button.css'
 
 const Button = ({ text, type, click }) => {
-  const [style, setStyle] = useState(null)
-
-  useEffect(() => {
-    setStyle({
-      backgroundColor: type === "primary" ? "#F0A500" : "#334756",
-      color: type === "primary" ? "#fff" : "#fff",
-      border: "none",
-      width: "50px",
-      height: "50px",
-      borderRadius: "10px",
-      cursor: "pointer",
-      fontSize: "1.5rem",
-      fontWeight: "bold",
-      outline: "none",
-      boxShadow: "0px 3px 10px rgba(0, 0, 0, 0.7)",
-    })
-  }, [])
+  const styled = {
+    backgroundColor: type === "primary" ? "#F0A500" : "#334756",
+    color: type === "primary" ? "#fff" : "#fff",
+  }
 
   const handleClick = (e) => {
     click(e)
   }
 
   return (
-    <input type="submit" className='btn' style={style} onClick={handleClick} value={text} />
+    <input type="submit" className='btn' style={styled} onClick={handleClick} value={text} />
   )
 }
 
