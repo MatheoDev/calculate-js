27e70a035483fec894793093cfd2c87b94f0a1a7
Matheo Deknuydt <matheodeknuydt@mbp-de-matheo.home>
2022-04-28 00:21:51 +0200

    review: review the calculate logics

--- src/Components/Calculate/Calculate.js
+++ src/Components/Calculate/Calculate.js
@@ -8,10 +8,10 @@ import './Calculate.css'
  * @param {Function} handleClick
  * @returns {JSX}
  */
-const Calculate = ({result, handleClick}) => {
+const Calculate = ({ result, handleClick }) => {
   return (
     <div className="calculate__display">
-      <input type="text" className="calculate__display--input" value={result} readOnly/>
+      <input type="text" className="calculate__display--input" value={result} readOnly />
       <div className='calculate__display--container'>
         {json.calculate.map((char, index) => (
           <Button key={index} text={char.value} type={char.type} click={handleClick} size={char.size} />
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Calculate from '../../Components/Calculate/Calculate'
 import './Home.css'
 
@@ -10,73 +10,74 @@ const Home = () => {
   const [calcul, setCalcul] = useState([])
   const [result, setResult] = useState('')
   const [history, setHistory] = useState([])
+  const [isChain, setIsChain] = useState(false)
 
   /**
-   * Logique de la calculatrice
+   * logics for the concatenation of the result
+   * @param {String} number 
+   * @returns 
+   */
+  const calculatorNumber = (number) => {
+    if (result && result.includes('.') && number === '.') {
+      return
+    }
+    if ((calcul.length && isChain)) {
+      setResult(number)
+      setIsChain(false)
+    } else if (result === 'Infinity'
+      || result === 'NaN' || result === '-Infinity'
+      || result === '-NaN' || result === '0') {
+      setCalcul([])
+      setResult(number)
+      setIsChain(false)
+    } else { setResult(result.concat(number)) }
+  }
+
+  /**
+   * logics for the operation of the result
+   * @param {String} operator 
+   */
+  const calculatorOperator = (operator) => {
+    switch (operator) {
+      case '+/-':
+        setResult(`${result * -1}`)
+        break
+      case '%':
+        setResult(`${result / 100}`)
+        break
+      case 'C':
+        setResult('')
+        setCalcul([])
+        break
+      case '=':
+        const resultCalcul = eval(calcul.join('').concat(result)).toString()
+        setResult(resultCalcul)
+        setCalcul([resultCalcul])
+        setHistory([...history, calcul.join('').concat(result).concat('=').concat(resultCalcul)])
+        break
+      default:
+        const resultCalculChaine = calcul.length ? eval(calcul.join('').concat(result)).toString() : ''
+        calcul.length ? setCalcul([eval(calcul.join('').concat(result)).toString(), operator]) : setCalcul([result, operator])
+        setResult(eval(calcul.join('').concat(result)).toString())
+        calcul.length && setHistory([...history, calcul.join('').concat(result).concat('=').concat(resultCalculChaine)])
+        setIsChain(true)
+        break
+    }
+  }
+
+  /**
+   * logique de la calculatrice
    * @param {*} value 
    * @returns 
    */
   const calculator = (value) => {
-    if (!/\d/.test(value) && !/[\.+=\-/*C%]/.test(value)) {
-      return
-    } else if (/[\d\.]/.test(value)) {
-      setCalcul([calcul.join('').concat(value)])
-      if (/[\d\.]/.test(calcul.splice(-1, 1))) {
-        setResult(result.concat(value))
-      } else {
-        setResult(value)
-      }
+    if (!/\d/.test(value) && !/[.+=\-/*C%]/.test(value)) {
       return
     }
-
-    if (calcul.length === 0) { return }
-
-    switch (value) {
-      case '%':
-        const splited = calcul.join('').split(/[\+\-*/]/)
-        if (splited.length === 1) {
-          setCalcul([calcul.join('') / 100])
-          setResult(calcul.join('') / 100)
-        } else {
-          const replaced = calcul.join('').replace(splited[0], '')
-          const pourcent = replaced.replace(splited[1], splited[1] / 100)
-          setCalcul([splited[0].concat(pourcent)])
-          setResult(splited[1] / 100)
-        }
-        break;
-      case 'C':
-        setCalcul([])
-        setResult('')
-        break;
-      case '=':
-        console.log(calcul.join(''))
-        setCalcul([eval(calcul.join(''))])
-        setResult(eval(calcul.join('')))
-        break;
-      case '+/-':
-        const splitedC = calcul.join('').split(/[\+\-*/]/)
-        if (splitedC.length === 1) { 
-          setCalcul([calcul.join('') * -1])
-          setResult(calcul.join('') * -1)
-        } else if (splitedC.length === 2 && splitedC[0] === '') { 
-          setCalcul([calcul.join('') * -1])
-          setResult(calcul.join('') * -1)
-        } else if (splitedC.length === 2 && !splitedC[1] === '') {
-          // const pourcent = replaced.replace(splited[1], splited[1] / 100) A REVOIR
-          // setCalcul([splitedC[0].concat(splitedC[1] * -1)])
-          // setResult(splitedC[1] * -1)
-        } else {
-          // const replaced = calcul.join('').replace(splitedC[0], '')
-          // const pourcent = replaced.replace(splitedC[1], splitedC[1] * -1) A REVOIR
-          // setCalcul([splitedC[0].concat(pourcent)])
-          // setResult(splitedC[1] * -1)
-        }
-        break;
-      default:
-        setCalcul([...calcul, value])
-        setResult(eval(calcul.join('')))
-        setHistory([...history, calcul.join('').concat(' = ').concat(result)])
-        break;
+    if (/[\d.]/.test(value)) {
+      calculatorNumber(value)
+    } else {
+      calculatorOperator(value)
     }
   }
 
